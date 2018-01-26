f769da4c81414a56b3cdaf4edeb823beee40d362
Okan <okan181995@gmail.com>
2018-01-27 02:01:46 +0300

    Hesap Makinesi kaynak kodu eklendi

---
+++ HESAP MAKİNESİ/frame/src/frame/Frame.java
@@ -0,0 +1,19 @@
+
+package frame;
+
+import javax.swing.JFrame;
+
+public class Frame extends JFrame {
+    
+    public String deneme;
+    
+    
+
+    public static void main(String[] args) {
+        
+        HesapMakinesi fr = new HesapMakinesi("Hesap Makinesi");
+
+    }
+
+    
+}
